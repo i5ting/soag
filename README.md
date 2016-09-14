287fe16be52937801af1eed966cf9581f16602c1
i5ting <i5ting@126.com>
2016-09-14 11:44:48 +0800

    lazycommmit, please ignore

---
+++ README.md
@@ -0,0 +1,105 @@
+# Soag
+
+Soag is a sequelize scaffold generator
+
+[![gitter][gitter-image]][gitter-url]
+[![NPM version][npm-image]][npm-url]
+
+
+![](doc/moa.jpg)
+
+> "Lost, like the Moa is lost" - Maori proverb
+
+## Features
+
+- support express && koa
+
+## Install
+
+- moag 1.x for moa2 v1
+
+```
+[sudo] npm install -g moag@1
+```
+ 
+- moag 2.x for moa2 v2
+
+```
+[sudo] npm install -g moag@2
+``` 
+
+## Usage
+
+### for koa 2 common function
+
+```
+moag student2 name:string password:string -k
+```
+
+### for koa2 async/await
+
+```
+moag cup name:string password:string -k -a
+```
+
+### for koa2 generator
+
+```
+moag book name:string password:string -k -g
+```
+
+### default for expresss
+
+    ➜  moag git:(master) moag
+    Moajs HELP:
+
+    moag: 【创建脚手架】 moag user name:string password:string uid:object
+
+    Have a good day! Moaer
+    
+### create scaffold user
+
+    moag user name:string password:object
+    
+    
+mongoose支持的data type基本如下：
+
+- String -> string
+- Number-> number
+- Date -> date
+- Boolean -> boolean
+- Buffer -> buffer
+- ObjectId -> object
+- Mixed  -> mixed
+- Array -> array
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+## History
+
+
+- v1.0.0 init
+
+
+## Welcome fork and feedback
+
+- write by `i5ting` shiren1118@126.com
+
+如有建议或意见，请在issue提问或邮件
+
+## License
+
+this repo is released under the [MIT
+License](http://www.opensource.org/licenses/MIT).
+
+
+[npm-image]: https://img.shields.io/npm/v/moag.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/moag
+[gitter-image]: https://badges.gitter.im/Join%20Chat.svg
+[gitter-url]: https://gitter.im/i5ting/moag?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge
---
+++ tpl/koa2-async/controllers/controller.js
@@ -0,0 +1,203 @@
+"use strict";
+
+/**
+ * Created by Moajs on {{created_at}}.
+ */
+ 
+var $models = require('mount-models')(__dirname);
+
+var {{entity}} = $models.{{model}};
+
+exports.list = async (ctx, next) => {
+  console.log(ctx.method + ' /{{models}} => list, query: ' + JSON.stringify(ctx.query));
+  try {
+    let {{models}} = await {{entity}}.getAllAsync();
+  
+    await ctx.render('{{models}}/index', {
+      {{models}} : {{models}}
+    })
+  } catch (err) {
+    return ctx.api_error(err);
+  }
+};
+
+exports.new = async (ctx, next) => {
+  console.log(ctx.method + ' /{{models}}/new => new, query: ' + JSON.stringify(ctx.query));
+  
+  await ctx.render('{{models}}/new', {
+    {{model}} : {
+      "_action" : "new"
+    }
+  });
+};
+
+exports.show = async (ctx, next) => {
+  console.log(ctx.method + ' /{{models}}/:id => show, query: ' + JSON.stringify(ctx.query) +
+    ', params: ' + JSON.stringify(ctx.params));
+    
+  try {
+    let id = ctx.params.id;
+    let {{model}} = await {{entity}}.getByIdAsync(id);
+  
+    console.log({{model}});
+  
+    await ctx.render('{{models}}/show', {
+      {{model}} : {{model}}
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
+};
+
+exports.edit = async (ctx, next) => {
+  console.log(ctx.method + ' /{{models}}/:id/edit => edit, query: ' + JSON.stringify(ctx.query) +
+    ', params: ' + JSON.stringify(ctx.params));
+
+  try {
+    let id = ctx.params.id;
+
+    let {{model}} = await {{entity}}.getByIdAsync(id);
+  
+    console.log({{model}});
+    {{model}}._action = 'edit';
+
+    await ctx.render('{{models}}/edit', {
+      {{model}} : {{model}}
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
+};
+
+exports.create = async (ctx, next) => {
+  console.log(ctx.method + ' /{{models}} => create, query: ' + JSON.stringify(ctx.query) +
+    ', params: ' + JSON.stringify(ctx.params) + ', body: ' + JSON.stringify(ctx.request.body));
+
+  try {
+    let {{model}} = await {{entity}}.createAsync({{keypair}});
+  
+    console.log({{model}});
+    await ctx.render('{{models}}/show', {
+      {{model}} : {{model}}
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
+};
+
+exports.update = async (ctx, next) => {
+  console.log(ctx.method + ' /{{models}}/:id => update, query: ' + JSON.stringify(ctx.query) +
+    ', params: ' + JSON.stringify(ctx.params) + ', body: ' + JSON.stringify(ctx.request.body));
+
+  try {
+    let id = ctx.params.id;
+
+    let {{model}} = await {{entity}}.updateByIdAsync(id,{{keypair}});
+  
+    ctx.body = ({
+      data:{
+        redirect : '/{{models}}/' + id
+      },
+      status:{
+        code : 0,
+        msg  : 'delete success!'
+      }
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
+};
+
+exports.destroy = async (ctx, next) => {
+  console.log(ctx.method + ' /{{models}}/:id => destroy, query: ' + JSON.stringify(ctx.query) +
+    ', params: ' + JSON.stringify(ctx.params) + ', body: ' + JSON.stringify(ctx.request.body));
+    
+  try {
+    let id = ctx.params.id;
+  
+    await {{entity}}.deleteByIdAsync(id);
+  
+    ctx.body = ({
+      data:{},
+      status:{
+        code : 0,
+        msg  : 'delete success!'
+      }
+    });
+  } catch (err) {
+    return ctx.api_error(err);
+  }
+};
+
+// -- custom
+
+// -- custom api
+exports.api = {
+  list: async (ctx, next) => {
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
+
+      let {{models}} = await {{entity}}.queryAsync({});
+    
+      await ctx.api({
+        {{models}} : {{models}}
+      })
+    } catch (err) {
+      return ctx.api_error(err);
+    }
+  },
+  show: async (ctx, next) => {
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
+      let id = ctx.params.{{model}}_id;
+
+      let {{model}} = await {{entity}}.getByIdAsync(id);
+    
+      await ctx.api({
+        {{model}} : {{model}}
+      });
+    } catch (err) {
+      return ctx.api_error(err);
+    }
+  },
+  create: async (ctx, next) => {
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
+
+      let {{model}} = await {{entity}}.createAsync({{keypair}});
+    
+      ctx.body = ({
+        {{model}} : {{model}}
+      });
+    } catch (err) {
+      return ctx.api_error(err);
+    }
+  },
+  update: async (ctx, next) => {
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
+      let id = ctx.params.{{model}}_id;
+    
+      let {{model}} = await {{entity}}.updateByIdAsync(id, {{keypair}});
+    
+      await ctx.api({
+        {{model}} : {{model}},
+        redirect : '/{{models}}/' + id
+      });
+    } catch (err) {
+      return ctx.api_error(err);
+    }
+  },
+  delete: async (ctx, next) => {
+    try {
+      let {{model}}_id = ctx.api_{{model}}._id;
+      let id = ctx.params.{{model}}_id;
+
+      await {{entity}}.deleteByIdAsync(id);
+    
+      await ctx.api({id: id});
+    } catch (err) {
+      return ctx.api_error(err);
+    }
+  }
+}
